cc4ffbbe74ddbfeab55be16547f45aab436211db
Your Name <you@example.com>
2022-09-01 09:58:57 +0900

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-etiz==rm!@e^dnk83m@_jj%l@qe_$9!(p&i-wbsna4m)sl8+l9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True#False#True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
